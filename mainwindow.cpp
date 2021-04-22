d680a576a9ff95297f7a6046ae8d7f43e769f9ec
Devooox <armolga29@gmail.com>
2021-04-22 12:51:26 +0300

    layouts

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), vertical_layout(this), up_horizontal_layout(this),
+      down_horizontal_layout(this), stacked_widget(this)
 
 {
-
+    vertical_layout.addLayout(&up_horizontal_layout, 1);
+    vertical_layout.addWidget(&stacked_widget, 3);
+    vertical_layout.addLayout(&down_horizontal_layout, 1);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,15 +2,22 @@
 #define MAINWINDOW_H
 
 #include <QWidget>
+#include <QStackedWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 class MainWindow : public QWidget
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
+    QVBoxLayout vertical_layout;
+    QHBoxLayout up_horizontal_layout;
+    QHBoxLayout down_horizontal_layout;
+    QStackedWidget stacked_widget;
 };
 #endif // MAINWINDOW_H
