79e5247541382bea906ffc35afdc2808086f2a15
Dmitry <karpenko.dima.s@mail.ru>
2021-04-22 17:06:25 +0300

    without bags

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,17 +12,15 @@ MainWindow::MainWindow(QWidget *parent)
     stacked_widget_.addWidget(main_menu_);
     //stacked_widget_.setCurrentIndex(0);
     stacked_widget_.addWidget(settings_);
-    stacked_widget_.setCurrentIndex(1);
+    stacked_widget_.setCurrentIndex(0);
     stacked_widget_.show();
     setLayout(stacked_widget_.layout());
 
-<<<<<<< HEAD
-=======
     auto close=[&](){
        this->close();
     };
-    connect(&main_menu_, &MainMenu::GoToExit, close);
->>>>>>> 02d91531f5b0f6c155d4b61a5dd03ad5d9b54875
+    connect(main_menu_, &MainMenu::GoToExit, close);
+
 }
 
 MainWindow::~MainWindow()
