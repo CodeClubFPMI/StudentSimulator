c518e2aacde05b8b8bbb4b1feb8bd3f0183c2a2c
Devooox <armolga29@gmail.com>
2021-04-30 20:47:23 +0300

    kekw

--- Utilities/utilities.h
+++ Utilities/utilities.h
@@ -2,12 +2,18 @@
 #define UTILITIES_H
 
 #include <QVector>
-#include "button.h"
+#include <QString>
 #include <QVBoxLayout>
+#include "button.h"
+#include "Game/parametrs.h"
 
 namespace menu_buttons{
     void add_buttons(QVector<Button *> &buttons, QVBoxLayout &layout);
     void change_buttons_style(QVector<Button *> &buttons, QVBoxLayout &layout);
+}
+
+namespace labels {
+    void set_label_parametrs(QLabel* label, QString background_color, QString text);
 }
 
 #endif // UTILITIES_H
